7d18bf1b2f60f8452590171ea0a846227a52eac7
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2022-08-03 12:30:19 -0300

    performs basic calculator functions

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,48 @@
 package Main;
 
+import java.util.Scanner;
+import Calculations.Calculator;
+
 public class Main {
     
     public static void main(String[] args) {
-        
-    }
+
+    Calculator calc = new Calculator();
+    Scanner scan = new Scanner(System.in);
+    int n;
+
+    do {
+      System.out.println("0 - Sair");
+      System.out.println("1 - Soma");
+      System.out.println("2 - Subtração");
+      System.out.println("3 - Multiplicação");
+      System.out.println("4 - Divisão");
+      System.out.println("5 - Quadrado do número");
+      System.out.println("Digite a opção desejada:");
+      n = scan.nextInt();
+      System.out.println();
+
+      switch (n) {
+        case 0:
+          break;
+        case 1:
+          calc.soma();
+          break;
+        case 2:
+          calc.subtracao();
+          break;
+        case 3:
+          calc.multiplicacao();
+          break;
+        case 4:
+          calc.divisao();
+          break;
+        case 5:
+          calc.quadrado();
+          break;
+        default:
+          System.out.println("Número inválido\n");
+      }
+    } while (n != 0);
+  }
 }
