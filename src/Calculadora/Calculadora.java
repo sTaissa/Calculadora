5a783b429e6150ac7bfdae55ae393e60ecd6f4e8
Taíssa Silva <61012521+sTaissa@users.noreply.github.com>
2022-08-05 15:21:49 -0300

    troca os dados para float

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -9,74 +9,74 @@ public class Calculadora {
 
     //soma os números até que seja digitado 0 e apresenta o resultado
     public void soma() {
-        int num, soma = 0;
+        float num, soma = 0;
 
         System.out.println("Digite os números para somar ou 0 para concluir a conta:");
 
         do {
-          num = scan.nextInt();
+          num = scan.nextFloat();
           soma += num;
         } while(num != 0);
 
-        System.out.println("O resultado da soma é: " + soma);
+        System.out.printf("O resultado da soma é: %.2f", soma);
         System.out.println();
     }
 
     //subtrai os números até que seja digitado 0 e apresenta o resultado
     public void subtracao() {
         System.out.println("Digite os números para subtrair ou 0 para concluir a conta:");
-        int num = scan.nextInt();
-        int sub = num;
+        float num = scan.nextFloat();
+        float sub = num;
         
         while(num != 0) {
-          num = scan.nextInt();
+          num = scan.nextFloat();
           sub -= num;
         }
 
-        System.out.println("O resultado da subtração é: " + sub);
+        System.out.printf("O resultado da subtração é: %.2f", sub);
         System.out.println();
     }
 
     //multiplica os números até que seja digitado 1 e apresenta o resultado
     public void multiplicacao() {
-        int mult = 0;
+        float mult = 1;
 
         System.out.println("Digite os números para multiplicar ou 1 para concluir a conta:");
-        int num = scan.nextInt();
+        float num = scan.nextFloat();
         
         while(num != 0) {
           mult *= num;
-          num = scan.nextInt();
+          num = scan.nextFloat();
         }
 
-        System.out.println("O resultado da multiplicação é: " + mult);
+        System.out.printf("O resultado da multiplicação é: %.2f", mult);
         System.out.println();
     }
 
     //divide os números até que seja digitado 0 e apresenta o resultado
     public void divisao() {
         System.out.println("Digite os números para dividir ou 0 para concluir a conta:");
-        int num = scan.nextInt();
-        int div = num;
+        float num = scan.nextFloat();
+        float div = num;
         
         while(num != 0) {
-          num = scan.nextInt();
+          num = scan.nextFloat();
           if(num == 0 ){
               break;
           }
           div /= num;
         }
 
-        System.out.println("O resultado da divisão é: " + div);
+        System.out.printf("O resultado da divisão é: %.2f\n", div);
         System.out.println();
     }
 
     //pede um número e mostra o seu quadrado
     public void quadrado() {
       System.out.println("Digite o número:");
-      int n = scan.nextInt();
+      float n = scan.nextFloat();
 
-      System.out.println("O quadrado do número é: " + (n * n));
+      System.out.printf("O quadrado do número é: %.2f\n", (n * n));
       System.out.println();
     }
 }
